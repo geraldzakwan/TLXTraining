55bcd5f452f7e921692bf6b21a7af721127cfb5c
geraldzakwan <geraldi.dzakwan@gmail.com>
2017-06-15 15:59:58 +0900

    stuck in lexicographic

--- srt_1.cpp
+++ srt_1.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
+#include <list>
 using namespace std;
+
+list<string> insert_name_ordered(list<string> ls, string name) {
+  if(ls.empty()) {
+    ls.push_back(name);
+  } else {
+    list<string>::const_iterator iterator;
+    for (iterator = ls.begin(); iterator != ls.end(); ++iterator) {
+      if((*iterator).length() >= name.length()) {
+          break;
+      }
+    }
+
+    // cout << "INSERT - START" << endl;
+    // list<string>::const_iterator iterator_2;
+    // cout << *iterator << endl;
+    // for (iterator_2 = iterator; iterator_2 != ls.end(); ++iterator_2) {
+    //   // cout << *iterator_2 << endl;
+    //   if((*iterator_2) >= name) {
+    //       break;
+    //   }
+    // }
+    //
+    // cout << "INSERT - END" << endl;
+
+    ls.insert(iterator_2, name);
+  }
+
+  return ls;
+}
 
 int main() {
   int n;
   cin >> n;
-  string name_list[n];
   string each_name;
+
+  list<string> ls;
 
   for (int i=0; i<n; i++) {
     cin >> each_name;
-
+    ls = insert_name_ordered(ls, each_name);
   }
 
-
+  list<string>::const_iterator iterator;
+  for (iterator = ls.begin(); iterator != ls.end(); ++iterator) {
+    cout << (*iterator) << endl;
+  }
 
   return 0;
 }
