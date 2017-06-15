e3e0e207677996322b4067ba14e4237c38f43e18
geraldzakwan <geraldi_dzakwan@yahoo.co.id>
2017-06-15 15:13:18 +0900

    add srt_1.cpp

---
+++ srt_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  string name_list[n];
+  string each_name;
+
+  for (int i=0; i<n; i++) {
+    cin >> each_name;
+
+  }
+
+
+
+  return 0;
+}
